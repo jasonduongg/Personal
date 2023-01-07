af2d5bce589aa8713ae7a8e788740679afa9c9d3
Jasonduong13 <Jasonduong@berkeley.edu>
2023-01-06 18:19:12 -0800

    3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Main from "./Main.jsx";
+import Project from "./Projects.jsx"
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Jason Duong
-      </h1>
-    </div>
+    <Router>
+        <Routes>
+          <Route exact path='/'  element={ <Main/>} />
+          <Route path='/projects' element={<Project/>} />
+        </Routes>
+      </Router>
   );
 }
 
---
+++ src/Projects.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const element = () => {
+  return (
+    <div>
+      <h1>
+        Projects
+      </h1>
+    </div>
+  )
+}
+
+export default element
