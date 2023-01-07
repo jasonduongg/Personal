0fd188ff281cfdd9c3d8dc8a84b691260560b740
Jasonduong13 <Jasonduong@berkeley.edu>
2023-01-06 18:26:41 -0800

    4

--- src/App.js
+++ src/App.js
@@ -6,13 +6,29 @@ import Project from "./Projects.jsx"
 
 function App() {
   return (
-    <Router>
-        <Routes>
-          <Route exact path='/'  element={ <Main/>} />
-          <Route path='/projects' element={<Project/>} />
-        </Routes>
-      </Router>
-  );
+    <div className="App">
+      <div>
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+        {/* <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch> */}
+      </div>
+        );
 }
 
 export default App;
